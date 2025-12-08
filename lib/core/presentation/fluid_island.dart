d8885af8f111e6626cc69fd5cc553bf33ed3a0e7
KenStarry <starrycodes@gmail.com>
2025-12-08 13:46:57 +0300

    Updated the Transition Builder to hide complexity

--- lib/core/presentation/fluid_island.dart
+++ lib/core/presentation/fluid_island.dart
@@ -7,6 +7,27 @@ class FluidIsland extends StatelessWidget {
 
   const FluidIsland({super.key, required this.child});
 
+  /// The user simply passes this to MaterialApp.builder
+  static TransitionBuilder builder({TransitionBuilder? upstreamBuilder}) {
+    return (BuildContext context, Widget? child) {
+      // 1. If they have another builder, run it first to get the widget tree
+      final Widget appContent = upstreamBuilder != null
+          ? upstreamBuilder(context, child)
+          : child ?? const SizedBox();
+
+      // 2. Wrap the result
+      return Overlay(
+        initialEntries: [
+          OverlayEntry(
+            builder: (context) {
+              return FluidIsland(child: appContent);
+            },
+          ),
+        ],
+      );
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return OverlayPortal(
